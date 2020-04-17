233782cdeb152e19991431b96e9b8525af147bc9
i.tairam_silva <italo.tairan@hotmail.com>
2020-04-16 22:40:48 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "gregao@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id= " + newSeller.getId());
+		
+		System.out.println("\n===Teste 05: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
 
 	}
 }
